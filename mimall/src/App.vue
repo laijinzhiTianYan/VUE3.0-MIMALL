237650914f27634547eac429cdcaa0c645f414a6
MrazZ <283523055@qq.com>
2020-04-29 16:50:01 +0800

    storage的封装，但存在初始化失败的问题

--- mimall/src/App.vue
+++ mimall/src/App.vue
@@ -5,15 +5,20 @@
 </template>
 
 <script>
+import storage from "./storage";
 export default {
-  data () {
-    return {
-      
-    }
+  data() {
+    return {};
+  },
+  mounted() {
+    // storage.setItem("a",1);  设置第一层级
+    // storage.setItem('user',{'a':1});  这个是覆盖整体式输入值
+    // storage.setItem("abc",{"a":2},"user");   这个针对某个模块输入值
+    // storage.clear("a");  清除第一层级
+    // storage.clear('a','user');  这个是清除某个模块中的值
+    storage.setItem("abc",{"a":2});
   }
-}
+};
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>

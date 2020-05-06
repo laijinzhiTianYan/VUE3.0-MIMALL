e53b66cad48b0a713fd6b84f3db39752e449551f
MrazZ <283523055@qq.com>
2020-05-06 20:19:08 +0800

    ServiceBar组件制作

---
+++ mimall/src/components/ServiceBar.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name: "ServiceBar",
+    data () {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

5418a5f96edc5600b9b70c3d59b971cba1209f5f
MrazZ <283523055@qq.com>
2020-06-05 23:55:54 +0800

    增加了通用modal组件

--- mimall/src/views/index.vue
+++ mimall/src/views/index.vue
@@ -4,6 +4,7 @@
     <!-- 这里就是首页的全部内容 -->
     <div class="index">
 
+
       <div class="container">
 
         <!-- 轮播区 -->
@@ -82,64 +83,55 @@
           <a href="/#/product/30"><img src="/imgs/banner-1.png" alt="banner-img"></a>
         </div>
 
+      </div>
 
-        <!-- 产品缩略区 -->
-        <div class="product-box">
-          <h2>手机</h2>
-          <div class="wrapper">
-            <div class="banner-left">
+      <!-- 产品缩略区 -->
+      <div class="product-box">
+          <div class="container">
+            <h2>手机</h2>
+            <div class="wrapper">
+              <div class="banner-left">
+                <img src="/imgs/mix-alpha.jpg" alt="">
+              </div>
 
-            </div>
-
-            <div class="list-box">
-              <div class="list" v-for = "(arr, i) in phoneList" :key = "i">
-                <div class="list-item" v-for = "(item ,j) in arr" :key = "j"><!-- 单个产品块 -->
-                  <span>新品</span>
-                  <div class="list-item-img">
-                    <img src="" alt="">
-                  </div>
-                  <div class="list-item-info">
-                    <h3>小米9</h3>
-                    <p>骁龙855，索尼4800万超广角微距</p>
-                    <p>2999元</p>
+              <div class="list-box">
+                <div class="list" v-for = "(arr, i) in phoneList" :key = "i">
+                  <div class="list-item" v-for = "(item ,j) in arr" :key = "j"><!-- 单个产品块 -->
+                    <!-- <span :class="{'new-pro' : j % 2 === 0}">新品</span>这是设置单一类的方法 -->
+                    <span :class="[ j % 2 === 0 ? 'new-pro' : 'kill-pro' ]">新品</span>
+                    <div class="list-item-img">
+                      <img :src="item.mainImage" alt="">
+                    </div>
+                    <div class="list-item-info">
+                      <h3>{{item.name}}</h3>
+                      <p class="list-item-describe">{{item.subTitle}}</p>
+                      <p class="list-item-price">￥{{item.price}}<a href="" class="list-item-cart"></a></p><!-- 其实这里要用过滤器格式化传入值的 -->
+                      
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-
-
-<!-- .product-box{
-  h2{}
-  .wrapper{
-    .banner-left{
-    }
-
-    .list-box{
-      .list{
-        .list-item{
-          span{}
-          .list-item-img{
-            img{}
-          }
-          .list-item-info{
-            h3{}
-            p{}
-          }
-        }
-      }
-    }
-  }
-} -->
-
-
 
       </div>
+
+
     </div>
     <service-bar></service-bar>
+    <modal 
+      title="提示" 
+      sureText="查看购物车" 
+      btnType="1" 
+      modalType="middle"
+      :showModal="showModal">
+
+      <template v-slot:body><!-- 新的slot语法，借助v-slot定义插入的位置 -->
+        <p>商品添加成功！</p>
+      </template>
+    </modal>
   </div>
-  
+
 </template>
 
 <script>
@@ -149,6 +141,7 @@
 import { Swiper, SwiperSlide, directive } from "vue-awesome-swiper";
 import "swiper/css/swiper.css";
 import ServiceBar from "../components/ServiceBar";
+import Modal from "../components/Modal";
 export default {
   name: "index",
   components: {
@@ -207,7 +200,7 @@ export default {
           img:"/imgs/slider/slide-5.jpg"
         }
       ],
-      
+
       //横向4个，纵向6组
       menuList:[//menuList储存手机，电话卡侧边栏的弹出信息数据
         [//每个里头有四个对象
@@ -253,20 +246,37 @@ export default {
         },
       ],
 
-      phoneList:[
-        [1,1,1,1],
-        [1,1,1,1]
+      phoneList:[//手机产品区的二维数组，其实也需要用对象来做一些数据引入
       ]
     }
+  },
+  mounted () {
+    console.log("current swiper instance object 当前swiper",this.swiper);
+    this.init();
   },
   computed: {
     swiper(){
       return this.$refs.indexSwiper.$swiper
     }
   },
-  mounted () {
-    console.log("current swiper instance object 当前swiper",this.swiper)
+  methods: {
+    init(){
+      this.axios.get("/products",{
+        params:{
+          // 后台要求的请求结构和识别码，注意是字符串，不然拿不到数据
+          categoryId:"100012",
+          // 请求加上pageSize即让服务端返回设置的条数，去掉以后即服务端发送全部数据
+          pageSize:14
+        }
+      }).then((res) => {
+        //slice不会改变原数组，splice会改变原数组
+        //由于得到的数据前6条是上面用的，只有后8条适合这里，所以要处理一下
+        this.list = res.list.slice(6,14);
+        this.phoneList = [res.list.slice(0,4),res.list.slice(4,8)]
+      })
+    }
   }
+
 };
 </script>
 
@@ -417,28 +427,128 @@ export default {
       }
     }
 
-    .product-box{
-      h2{}
-      .wrapper{
-        .banner-left{
+  }
+
+  .product-box{
+      outline: dashed .1px red;
+      background:rgba(245,245,245,1);
+      .container{//吧内容放到安全区内，好设置整体的背景色
+      position: relative;
+      height: 740px;
+        h2{//大标题
+        position: absolute;
+        top: 30px;
+        left: 0;
         }
-    
-        .list-box{
-          .list{
-            .list-item{
-              span{}
-              .list-item-img{
-                img{}
+        .wrapper{//n内容区
+          .banner-left{//左边banner
+            position: absolute;
+            top: 71px;
+            left: 0;
+            width: 224px;
+            height: 619px;
+            transition: all .2s linear;
+            img{
+              width: 100%;
+              height: 100%;
+            }
+            &:hover{
+              margin-top: -8px;
+              box-shadow: 4px 4px 15px lightgray;
+              transition: all .2s linear;
+            }
+          }
+
+          .list-box{//右侧item区
+            outline: dashed .1px blue;
+            position: absolute;
+            top: 71px;
+            left: 240px;
+
+            .list{//每行
+              outline: dashed .1px green;
+              @include flex();
+              height: 302px;
+              width: 986px;
+              margin-bottom: 14px;
+              &:last-child{
+                margin-bottom: 0;//最后一组list不需要底边距
               }
-              .list-item-info{
-                h3{}
-                p{}
+              .list-item{//每个元素
+                outline: dashed .1px brown;
+                position: relative;
+                width: 236px;
+                height: 302px;
+                text-align: center;//为了定位span这种需要居中的，
+                transition: all .2s linear;
+
+                span{//如果用绝对定位，可能会随内容变化出现位移偏差，
+                  width: 67px;
+                  height: 24px;
+                  font-size: 14px;
+                  line-height: 24px;
+                  color: $colorG;
+                  &.new-pro{
+                    background-color: #7ECF68;
+                  }
+                  &.kill-pro{
+                    background-color: #E82626;
+                  }
+                }
+                .list-item-img{
+                  outline: dashed .1px blue;
+                  width: 190px;
+                  height: 195px;
+                  margin: 0 auto;//调整图片容器的位置
+                  @include flex(center,center);//调整图片的位置
+                  img{
+                    outline: dashed .1px red;
+                    height: 75%;
+                  }
+                }
+                .list-item-info{
+                  line-height: 14px;
+                  text-align: center;
+                  @include flex();
+                  flex-direction: column;
+                  h3{
+                    color: #333333;
+                    font-size: 14px;
+                    position: absolute;//去掉绝对定位，如果父元素设置text-align也能居中，但是位置就要用margin定位了
+                    bottom: 75px;
+                  }
+                  .list-item-describe{
+                    color: #999999;
+                    font-size: 12px;
+                    position: absolute;
+                    bottom: 56px;
+                  }
+                  .list-item-price{
+                    color: #F20A0A;
+                    font-size: 14px;
+                    font-weight: bold;
+                    position: absolute;
+                    bottom: 30px;
+                    .list-item-cart{
+                      outline: dashed .1px ;
+                      @include bgImg(16,16,"/imgs/icon-cart-hover.png");//z注意传参时候，要看原函数需不需要带单位
+                      vertical-align: bottom;//文字和图片视差变小
+                    }
+                  }
+                  
+                }
+
+                &:hover{
+                  margin-top: -8px;
+                  box-shadow: 4px 4px 15px lightgray;
+                  transition: all .2s linear;
+                }
               }
             }
           }
-        }
+          }
       }
 }
-  }
+
 }
 </style>

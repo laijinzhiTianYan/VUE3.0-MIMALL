09ce71887182d8c21bd7c33f5452c2054cd64ec6
MrazZ <283523055@qq.com>
2020-06-06 23:58:01 +0800

    更新

--- mimall/src/components/Modal.vue
+++ mimall/src/components/Modal.vue
@@ -1,36 +1,49 @@
 <template>
-  <div class="modal" v-show="showModal">
-    <!-- 这个v-show也可以放在其他组件中调取，但要用动画的话可能失效，所以放这里 -->
+  <div
+    class="modal"
+    v-show="showModal"
+  ><!-- 这个v-show也可以放在其他组件中调取，但要用动画的话可能失效，所以放这里 -->
+    
     <div class="mask">
       <!-- 弹框 -->
       <div class="modal-dialog">
         <div class="modal-dialog-header">
           <span>标题</span>
-          <a href="javascript:;" class="icon-close"></a>
+          <a
+            href="javascript:;"
+            class="icon-close"
+          ></a>
         </div>
         <div class="modal-dialog-body">
           <slot name="body"></slot>
         </div>
         <div class="modal-dialog-footer">
-          <a href="javascript:;" class="btn">确定</a>
-          <a href="javascript:;" class="btn">取消</a>
+          <a
+            href="javascript:;"
+            class="btn"
+          >确定</a>
+          <a
+            href="javascript:;"
+            class="btn"
+          >取消</a>
         </div>
       </div>
     </div>
   </div>
 </template>
 
+
 <script>
 export default {
   name: "modal",
-  props: {
+  props: {//其他模块使用组件时自动识别并判断类型
     //弹框类型：小small、中middle、大large、表单form
     modalType: {
       type: String,
       default: "form"
     },
     //弹框标题
-    title: "String",
+    title: String, //注意类型可不是字符串，不要写成"String"
     //按钮类型，确定或者取消按钮，或者二者都有
     btnType: String,
     sureText: {
@@ -46,4 +59,8 @@ export default {
   }
 };
 </script>
-<style></style>
+<style lang="scss">//每个style都设置个lang
+@import "../assets/scss/config.scss";
+@import "../assets/scss/mixin.scss";
+@import "../assets/scss/modal.scss";
+</style>

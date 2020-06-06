1ca9b896795b3faa27a3a1e9bcca54e9ea4f811d
MrazZ <283523055@qq.com>
2020-06-07 00:37:58 +0800

    更新弹框组件

--- mimall/src/components/Modal.vue
+++ mimall/src/components/Modal.vue
@@ -2,22 +2,25 @@
   <div
     class="modal"
     v-show="showModal"
-  ><!-- 这个v-show也可以放在其他组件中调取，但要用动画的话可能失效，所以放这里 -->
-    
-    <div class="mask">
-      <!-- 弹框 -->
-      <div class="modal-dialog">
-        <div class="modal-dialog-header">
-          <span>标题</span>
-          <a
-            href="javascript:;"
-            class="icon-close"
-          ></a>
-        </div>
-        <div class="modal-dialog-body">
-          <slot name="body"></slot>
-        </div>
-        <div class="modal-dialog-footer">
+  >
+    <!-- 这个v-show也可以放在其他组件中调取，但要用动画的话可能失效，所以放这里 -->
+
+    <div class="mask"></div><!-- 注意这个遮罩层html结构和css，都要和内容并联放置，不然会影响 -->
+    <!-- 弹框 -->
+    <div class="modal-dialog">
+      <div class="modal-dialog-header">
+        <span>标题</span>
+        <a
+          href="javascript:;"
+          class="icon-close"
+        ></a>
+      </div>
+      <div class="modal-dialog-body">
+        <slot name="body"></slot>
+      </div>
+      <div class="modal-dialog-footer">
+        <!-- 对一个组件中css样式有两种方式实现，一种是在本组件中引入，另一种是挂载在全局App.vue中引入，比如这里的btn -->
+        <div class="btn-grup">
           <a
             href="javascript:;"
             class="btn"
@@ -27,6 +30,7 @@
             class="btn"
           >取消</a>
         </div>
+
       </div>
     </div>
   </div>
@@ -36,7 +40,8 @@
 <script>
 export default {
   name: "modal",
-  props: {//其他模块使用组件时自动识别并判断类型
+  props: {
+    //其他模块使用组件时自动识别并判断类型
     //弹框类型：小small、中middle、大large、表单form
     modalType: {
       type: String,
@@ -59,7 +64,8 @@ export default {
   }
 };
 </script>
-<style lang="scss">//每个style都设置个lang
+<style lang="scss">
+//每个style都设置个lang
 @import "../assets/scss/config.scss";
 @import "../assets/scss/mixin.scss";
 @import "../assets/scss/modal.scss";

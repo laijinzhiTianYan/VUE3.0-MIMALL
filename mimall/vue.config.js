c35e248d516dabdf4d1fc9ba4cf46fb953944182
MrazZ <283523055@qq.com>
2020-06-07 22:40:40 +0800

    格式化

--- mimall/vue.config.js
+++ mimall/vue.config.js
@@ -4,16 +4,16 @@ module.exports = {
         port:8080,
         // 设置了proxy就不用jsonp和cros跨域了，
         // 连带着main中的axios.default.baseURL也要更改
-          proxy:{
-              '/api':{
-                 //  真实的接口代理
-                //  记得重设代理后要重新启动下项目
-                  target:'http://mall-pre.springboot.cn',
-                  changeOrigin:true,
-                  pathRewrite:{
-                      '/api':''
-                  }
-              }
-        }
+        proxy:{
+            '/api':{
+               //  真实的接口代理
+              //  记得重设代理后要重新启动下项目
+                target:'http://mall-pre.springboot.cn',
+                changeOrigin:true,
+                pathRewrite:{
+                    '/api':''
+                }
+            }
+        
     }
 }

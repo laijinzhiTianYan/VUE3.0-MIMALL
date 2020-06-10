4726247fbb8ec3d69a2725d3d85ea7c75ed8fabb
MrazZ <283523055@qq.com>
2020-06-10 22:29:48 +0800

    更新

--- mimall/src/views/login.vue
+++ mimall/src/views/login.vue
@@ -1,5 +1,35 @@
 <template>
-  <div></div>
+  <div>
+    这是login视图层
+    <div class="login">
+      <div class="login-header">
+        <div class="container">
+          <div class="login-header-logo">
+            <a href=""></a>
+          </div>
+          <div class="login-header-title">
+            <h1>小米商城</h1>
+            <span>让每个人都享受科技的乐趣</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="login-banner">
+        <img src="" alt="">
+      </div>
+
+      <div class="login-form-area">
+        <div class="login-form-area-tit">
+          <ul>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+
+        div.login-form-area-input
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +37,6 @@ export default {
   name: "login"
 };
 </script>
+<style lang="scss" scoped>
+// @import "../assets/scss/base.scss";注意几个@import已经在app.vue里头引入了
+</style>
